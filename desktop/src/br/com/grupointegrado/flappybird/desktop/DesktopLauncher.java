160f5bb48a3c661379f6ac0e900dd9f63ecbd97b
Rebeca Franco Hernandes <rebecahernandes@outlook.com>
2015-09-28 23:36:48 -0300

    no message

---
+++ desktop/src/br/com/grupointegrado/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.grupointegrado.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.grupointegrado.flappybird.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
